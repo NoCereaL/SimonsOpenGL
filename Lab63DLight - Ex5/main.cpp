c114ea0885fc5136499201deeebfb9a79fc88799
Simon Moyana <therealskm@hotmail.com>
2022-11-13 14:31:11 +0000

    Lab 6 Ex 5 Upload & Complete

---
+++ Lab63DLight - Ex5/main.cpp
@@ -0,0 +1,232 @@
+// Code adapted from www.learnopengl.com, www.glfw.org
+
+#include <iostream>
+
+// GLEW
+#define GLEW_STATIC
+#include <GL/glew.h>
+
+// GLFW
+#include <GLFW/glfw3.h>
+
+// GLM Mathematics
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+// Other includes
+#include "Shader.h"
+
+// Window dimensions
+const GLuint WIDTH = 640, HEIGHT = 640;
+
+// Camera
+glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+
+// Light attributes
+glm::vec3 lightPos(1.0f, 0.0f, 1.0f);
+
+// The MAIN function, from here we start the application and run the game loop
+int main(void)
+{
+	//++++create a glfw window+++++++++++++++++++++++++++++++++++++++
+	GLFWwindow* window;
+
+	GLfloat time;
+
+	if (!glfwInit()) //Initialize the library
+		return -1;
+
+	window = glfwCreateWindow(WIDTH, HEIGHT, "Simon's 3D OpenGL Window", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);//Make the window's context current
+
+								   //++++Initialize GLEW to setup the OpenGL Function pointers+++++++
+	glewExperimental = GL_TRUE;
+	glewInit();
+
+	//++++Define the viewport dimensions++++++++++++++++++++++++++++
+	glViewport(0, 0, HEIGHT, HEIGHT);
+
+	// Setup OpenGL options
+	glEnable(GL_DEPTH_TEST);
+
+	// Set up vertex data (and buffer(s)) and attribute pointers
+	GLfloat vertices[] = {
+		 //position             //normal vector
+		-0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+		0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+		0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+		0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+		-0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+
+		-0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+		0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+		0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+		0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+		-0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+
+		-0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+		-0.5f,  0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+		-0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+		-0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+		-0.5f, -0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+		-0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+
+		0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+		0.5f,  0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+		0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+		0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+		0.5f, -0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+		0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+
+		-0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+		0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+		0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+		0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+
+		-0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
+		0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
+		0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+		0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+		-0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+		-0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f
+	};
+	// First, set the container's VAO (and VBO)
+	GLuint VBO, VAO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glBindVertexArray(VAO);
+	// Position attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)0);
+	glEnableVertexAttribArray(0);
+	// Normal attribute
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(1);
+	glBindVertexArray(0);
+
+	// Build and compile our shader program
+	GLuint shaderProgram = initShader("vert.glsl", "frag.glsl");
+
+	// Game loop
+	while (!glfwWindowShouldClose(window))
+	{
+		// Check if any events have been activiated (key pressed, mouse moved etc.) and call corresponding response functions
+		glfwPollEvents();
+
+		// Clear the colorbuffer
+		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		time = glfwGetTime();
+		//Ex5 translate camera back and forth
+		//GLfloat dx = glm::abs(glm::sin(time) * 1.5f);
+		//cameraPos = glm::vec3(0.0f, 0.0f, dx+2.0f);
+
+		//Ex5 translate up and down
+		//GLfloat dx = glm::abs(glm::sin(time) * 0.5f);
+		//cameraFront = glm::vec3(0.0f, dx-0.25f, -1.0f);
+
+		//Ex5 different up vectors
+		GLfloat dx = glm::abs(glm::sin(time) * 3.0f);
+		//cameraUp = glm::vec3(1.0f, 2.0f, 0.0f);
+		cameraUp = glm::vec3(time, 2.0f, dx);
+
+		// Use cooresponding shader when setting uniforms/drawing objects
+		glUseProgram(shaderProgram);
+		GLint objectColorLoc = glGetUniformLocation(shaderProgram, "objectColor");
+		GLint lightColorLoc = glGetUniformLocation(shaderProgram, "lightColor");
+		GLint lightPosLoc = glGetUniformLocation(shaderProgram, "lightPos");
+		GLint viewPosLoc = glGetUniformLocation(shaderProgram, "viewPos");
+		glUniform3f(objectColorLoc, 1.0f, 0.0f, 0.0f);
+		glUniform3f(lightColorLoc, 1.0f, 1.0f, 1.0f);
+		glUniform3f(lightPosLoc, lightPos.x, lightPos.y, lightPos.z);
+		glUniform3f(viewPosLoc, cameraPos.x, cameraPos.y, cameraPos.z);
+
+		// Create camera transformations
+		glm::mat4 view;
+		glm::mat4 projection;
+		view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
+		projection = glm::perspective(45.0f, (GLfloat)WIDTH / (GLfloat)HEIGHT, 0.1f, 100.0f);
+
+		//Ex5 Alt translate back and forth using FOV (Field of View)
+		//GLfloat dx = glm::abs(glm::sin(time) * 0.5f);
+		//projection = glm::perspective(dx + 45, (GLfloat)WIDTH / (GLfloat)HEIGHT, 0.1f, 100.0f);
+
+		// Get the uniform locations
+		GLint modelLoc = glGetUniformLocation(shaderProgram, "model");
+		GLint viewLoc = glGetUniformLocation(shaderProgram, "view");
+		GLint projLoc = glGetUniformLocation(shaderProgram, "projection");
+		// Pass the matrices to the shader
+		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+		glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
+
+		// Draw the object
+		glBindVertexArray(VAO);
+		glm::mat4 model;
+		model = glm::rotate(model, (GLfloat)glfwGetTime() * 1.0f, glm::vec3(0.0f, 1.0f, 0.0f));
+
+		//Ex1 move Cub (0.5,0.5,0.5)
+		//model = glm::translate(model, glm::vec3(0.5f, 0.5f, 0.5f));
+
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+		glDrawArrays(GL_TRIANGLES, 0, 72);
+
+		//Ex2 Draw second cube
+		/*glBindVertexArray(VAO);
+		glm::mat4 model2;
+		model2 = glm::translate(model2, glm::vec3(-0.5f,-0.5f,-0.5f));
+		model2 = glm::rotate(model2, -(GLfloat)glfwGetTime() * 1.0f, glm::vec3(0.0f, 1.0f, 0.0f));
+
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model2));
+		glDrawArrays(GL_TRIANGLES, 0, 72);
+		*/
+
+		//Ex3 Draw Second Cube Still
+		/*glBindVertexArray(VAO);
+		glm::mat4 model2;
+		model2 = glm::translate(model2, glm::vec3(-0.5f, -0.5f, -0.5f));
+		model2 = glm::rotate(model2, -0.785f, glm::vec3(0.0f, 1.0f, 0.0f));
+
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model2));
+		glDrawArrays(GL_TRIANGLES, 0, 72);
+		*/
+
+		//Ex4 Draw Second Cube Different Size
+		/*glBindVertexArray(VAO);
+		glm::mat4 model2;
+		model2 = glm::scale(model, glm::vec3(0.5f,0.5f,0.5f));
+		model2 = glm::translate(model2, glm::vec3(-1.5f, -1.5f, -1.5f));
+		model2 = glm::rotate(model2, -(GLfloat)glfwGetTime() * 1.0f, glm::vec3(0.0f, 1.0f, 0.0f));
+		model2 = glm::translate(model2, glm::vec3(-1.5f, -1.5f, -1.5f));
+
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model2));
+		glDrawArrays(GL_TRIANGLES, 0, 72);
+		*/
+
+		glBindVertexArray(0);
+
+		// Swap the screen buffers
+		glfwSwapBuffers(window);
+	}
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	// Terminate GLFW, clearing any resources allocated by GLFW.
+	glfwTerminate();
+	return 0;
+}
